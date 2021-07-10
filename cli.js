c0e29e13e3b588adda9f1b75ac443596ef2375d8
alestor123 <alestoraldous@gmail.com>
2021-07-10 14:15:14 +0530

    added simple cli

--- cli.js
+++ cli.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+const chalk = require('chalk')
+const { readFileSync, writeFileSync } = require('fs')
+const pcj = require('./App')
+var argv = require('minimist')(process.argv.slice(2));
+try {
+var infille = argv.i || argv.input,
+    outFile = argv.o || argv.output
+    writeFileSync(outFile, pcj(JSON.parse(readFileSync(infille).toString()), outFile,argv.t || argv.tag ))
+}
+catch(e) {
+console.log(chalk.redBright.bold('Oops we got a problem'))
+}
